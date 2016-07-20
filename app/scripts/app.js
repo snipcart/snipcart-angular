4873c832b1280ca33d82caec009f010c36c57f22
Gabriel Robert <grobert@spektrummedia.com>
2016-07-19 20:23:41 -0400

    Initial commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name snipcartAngularApp
+ * @description
+ * # snipcartAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('snipcartAngularApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name snipcartAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the snipcartAngularApp
+ */
+angular.module('snipcartAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
