b5806a111748350fa6e9404bc4237f0ffbe8341d
Gabriel Robert <grobert@spektrummedia.com>
2016-07-21 15:35:59 -0400

    Removing useless console.log

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,5 @@ angular.module('snipcartAngularApp')
   .controller('MainCtrl', function ($scope, $http) {
     $http.get('/data/products.json').then(function(res) {
   		$scope.products = res.data;
-  		console.log(res.data)
     });
   });
