0df100c3b26e7fe3eea5d3bfc980b68df8cb5577
Gabriel Robert <grobert@spektrummedia.com>
2016-07-21 15:32:51 -0400

    Grunt build add data folder to dist folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -380,7 +380,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'data/*.*'
           ]
         }, {
           expand: true,
